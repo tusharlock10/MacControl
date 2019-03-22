86b8d80d03c158a504171407d50bc4f488093519
Tushar Jain <tusharlock10@gmail.com>
2019-03-23 01:12:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import osascript as AS
 import os
 from fire import Fire
 from tj import color_text as ct
+import subprocess
 
 error = None
 warn = None
 
 
 def clr(num, warn=None):
-    color="BLUE"
+    color = "BLUE"
     if warn:
-        color="GREEN"
+        color = "GREEN"
     return ct(str(num), text_color=color, bold=True)
 
 
@@ -24,7 +25,8 @@ def get_num(num):
         return None
 
     if num < 0:
-        error = "Enter only a +ve integer or decimal number\nYou entered - %s" % clr(num)
+        error = "Enter only a +ve integer or decimal number\nYou entered - %s" % clr(
+            num)
         return None
     if 0.0 <= num <= 1.0:
         pass
@@ -32,12 +34,60 @@ def get_num(num):
         num = num/100
     else:
         num = 1.0
-        warn = "Number should only be between 0-1 or 0-100\nYou entered - %s , so I took it as 1" % clr(n,1)
+        warn = "Number should only be between 0-1 or 0-100\nYou entered - %s , so I took it as 1" % clr(
+            n, 1)
     return num
 
 
-def vol(vol):
-    AS.osascript("set volume output volume %s" % vol)
+def volume(arg='output', vol=None):
+    global error, warn
+    arg=arg.lower()
+    if arg in ['mute', 'muted', 'unmute']:
+        vol=1
+
+
+    if vol:
+        vol = get_num(vol)
+        if 0.0 < vol < 1.0:
+            vol = round(vol*100, None)
+
+        
+        if arg in ['s','speaker','speakers','output','o']:
+            arg='output volume %s' % vol
+        elif arg in ['i', 'mic','microphone','mics','microphones', 'input', 'i']:
+            arg='input volume %s' % vol
+        elif arg in ['a', 'alert','notify', 'notification']:
+            arg='alert volume %s' % vol
+        elif arg in ['mute', 'muted']:
+            arg="output muted true"
+        elif arg in ['unmute']:
+            arg="output muted false"
+        else:
+            arg='output volume %s' % vol
+            warn="Enter available options to control volume are\n * speaker\n * mic\n * alert\n\
+mute or unmute\neg. control volume mute\neg. control volume mic 20"
+
+        cmd = "osascript -e 'set volume %s'" % arg
+        os.system(cmd)
+        print(ct('SPEAKER VOLUME is now set to %s' %
+                 vol, text_color='OLIVE', bold=1))
+
+    else:
+        cmd = "osascript -e 'get volume settings'"
+        process = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+        output = process.stdout.decode().strip('\n').split(', ')
+        o = [i.split(':')[1] for i in output]
+
+        x = ''
+        if o[3] == 'true':
+            x = ' but speakers are ' + \
+                ct('* MUTED *', text_color='GREEN', bold=1)
+
+        print(ct('SPEAKER VOLUME | %s' %
+                 o[0]+' %', text_color='OLIVE', bold=1)+x)
+        print(ct('  MIC VOLUME   | %s' %
+                 o[1]+' %', text_color='YELLOW', bold=1))
+        print(ct(' ALERT VOLUME  | %s' % o[2]+' %', text_color='BLUE', bold=1))
 
 
 def keyboard(num):
@@ -50,23 +100,23 @@ def brightness(num=None):
     if num:
         n = get_num(num)
         if n:
-            if n<0.06:
-                n=0.000001
-                choice = input('This will turn off the display, are you sure (Y/N): ').lower()
-                if choice not in ['y','yes']:
+            if n < 0.06:
+                n = 0.000001
+                choice = input(
+                    'This will turn off the display, are you sure (Y/N): ').lower()
+                if choice not in ['y', 'yes']:
                     print('Aborting...')
                     return None
             os.system("./db %s" % n)
-    
+
     else:
         os.system("./db")
         print()
 
+
 def backlight(num=None):
     brightness(num)
     print()
-
-
 
 
 if __name__ == '__main__':
